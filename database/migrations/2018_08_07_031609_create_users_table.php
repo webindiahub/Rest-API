b269c9123ce3975a42639250edb7b8fdae9d2a40
gautam-s04 <gautam.bhadauria@crownstack.com>
2018-08-08 07:52:16 +0530

    Added new field for password

--- database/migrations/2018_08_07_031609_create_users_table.php
+++ database/migrations/2018_08_07_031609_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('password');
             $table->enum('gender', ['male', 'female']);
             $table->string('bio')->nullable();
             $table->timestamps();
