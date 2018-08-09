f1bacdfc3d8c23cbce0ac95d1ad1538f4806ab91
gautam-s04 <gautam.bhadauria@crownstack.com>
2018-08-09 07:51:45 +0530

    Added: Session table for user session managment

---
+++ database/migrations/2018_08_09_020919_create_session_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('session', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('session_id')->unique();
+            $table->unsignedInteger('user_id');
+            $table->string('ip_address')->nullable();
+            $table->string('user_agent')->nulable();
+            $table->string('device-token')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('session');
+    }
+}
